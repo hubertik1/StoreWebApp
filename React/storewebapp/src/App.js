bd01a7d21b8c7fc42893dfd5bb7707edf3c84547
Hubert Marciniak <hubertik@hotmail.com>
2025-04-12 14:23:31 +0200

    add more products

--- React/storewebapp/src/App.js
+++ React/storewebapp/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="header">
-          <h1>My Online Shop</h1>
+          <h1>Sklep Wszystko i Nic</h1>
         </header>
         <div className="container">
           <div className="products-list">
